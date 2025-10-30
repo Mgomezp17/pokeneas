02d0218122936780ccfdd4ec2a428800a4719885
Julian Valencia <valenciajuliann@hotmail.com>
2025-10-30 16:31:31 -0500

    add pokeneas

---
+++ src/constants/pokeNeas.js
@@ -0,0 +1,58 @@
+const pokeneas = [
+  {
+    id: 1,
+    name: "Caterpilas",
+    height: 35,
+    hability: "Estar en la jugada",
+    image: "https://storage.googleapis.com/pokeneas-marcoaurelio-julian/Caterpilas.png",
+    fraseFilosofica: "Camarón que se duerme se lo lleva la corriente"
+  },
+  {
+    id: 2,
+    name: "Mero Pato",
+    height: 60,
+    hability: "Hacerse el guevón",
+    image: "https://storage.googleapis.com/pokeneas-marcoaurelio-julian/MeroPato.png",
+    fraseFilosofica: "El que no llora no mama, y el que no mama se muere"
+  },
+  {
+    id: 3,
+    name: "Pikachimba",
+    height: 40,
+    hability: "Dar corriente",
+    image: "https://storage.googleapis.com/pokeneas-marcoaurelio-julian/Pikachimba.png",
+    fraseFilosofica: "Más vale pájaro en mano que cien volando"
+  },
+  {
+    id: 4,
+    name: "Charmanderecho",
+    height: 55,
+    hability: "Calentar el ambiente",
+    image: "https://storage.googleapis.com/pokeneas-marcoaurelio-julian/Charmanderecho.png",
+    fraseFilosofica: "El que juega con fuego se quema, parcero"
+  },
+  {
+    id: 5,
+    name: "Bulbasapo",
+    height: 70,
+    hability: "No ser sapo",
+    image: "https://storage.googleapis.com/pokeneas-marcoaurelio-julian/Bulbasapo.png",
+    fraseFilosofica: "Loro viejo no aprende a hablar pero tampoco sapea"
+  },
+  {
+    id: 6,
+    name: "Squirtman",
+    height: 50,
+    hability: "Echar agua a la fiesta",
+    image: "https://storage.googleapis.com/pokeneas-marcoaurelio-julian/Squirtleman.png",
+    fraseFilosofica: "Agua que no has de beber, déjala correr"
+  },
+  {
+    id: 7,
+    name: "Jigglyparcero", 
+    height: 45,
+    hability: "Dormir el enemigo",
+    image: "https://storage.googleapis.com/pokeneas-marcoaurelio-julian/Jigglyparcero.png",
+    fraseFilosofica: "A palabras necias, oídos sordos"
+  }
+];
